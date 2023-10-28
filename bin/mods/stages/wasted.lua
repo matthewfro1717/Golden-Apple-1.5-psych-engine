9d552aea94793632e7d06795b5e709173ca27bc6
matthewfro1717 <114807610+matthewfro1717@users.noreply.github.com>
2023-10-28 09:47:11 -0400

    Create wasted.lua

---
+++ bin/mods/stages/wasted.lua
@@ -0,0 +1,17 @@
+function onCreate()
+	makeLuaSprite('void', 'joke/wasted/void', -1475, -775);
+	scaleObject('void', 1.75, 1.75);
+	addLuaSprite('void', false)
+  
+	makeLuaSprite('text', 'joke/wasted/text', -1475, -775);
+	scaleObject('text', 1.75, 1.75);
+	addLuaSprite('text', false)
+  
+	makeLuaSprite('browninternetaccess', 'joke/wasted/browninternetaccess', -1475, -775);
+	scaleObject('browninternetaccess', 1.75, 1.75);
+	addLuaSprite('browninternetaccess', false)
+  
+	makeLuaSprite('black bar', 'joke/wasted/black bar', -1475, -775);
+	scaleObject('black bar', 1.75, 1.75);
+	addLuaSprite('black bar', false)
+end
